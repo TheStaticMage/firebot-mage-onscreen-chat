4bfb6c87d3d41192c557c12dcb44dd7560e248b2
TheStaticMage <199814801+TheStaticMage@users.noreply.github.com>
2025-10-04 14:24:44 +0000

    Bump version to 1.0.1

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ export let firebot: RunRequest<Params>;
 export let logger: Logger;
 export let params: Params;
 
-const scriptVersion = '1.0.0';
+const scriptVersion = '1.0.1';
 
 export const IntegrationConstants = {
     IntegrationId: 'mage-onscreen-chat',
